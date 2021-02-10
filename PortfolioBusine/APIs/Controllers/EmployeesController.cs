6dfd08387f97cec9c7c77f53e757633df58f8061
Ryan McDowell <ryanmcdowelldeveloper@gmail.com>
2021-02-09 21:30:00 -0500

    first commit

---
+++ PortfolioBusine/APIs/Controllers/EmployeesController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DataAccess.ModelsAzure;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APIs.Controllers
+{
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        [Route("GetEmployees")]
+        [HttpGet]
+        public IEnumerable<Employee> GetCampaignNameFromProgram()
+        {
+            var employees = new List<Employee>();
+
+            using (var dbContext = new PortfolioProjectContext())
+            {
+                employees = dbContext.Employees.ToList();
+            }
+
+            return employees;
+        }
+    }
+}
